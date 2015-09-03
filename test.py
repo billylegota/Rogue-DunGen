e24e2bce5294aadac77bed90751cb2912ddb58ed
billylegota <billy@texasblounts.com>
2015-09-02 22:25:13 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+import generator
+
+Test = generator.Level("Test", 150, 75)
+Test.placeRooms(150)
+
+def printLevel(level):
+  output = ""
+  for y in range(level.y):
+    for x in range(level.x):
+      if level.level[x][y] == 0:
+        output += "#"
+      elif level.level[x][y] == 1:
+        output += " "
+    output += "\n"
+  return output
+  
+print printLevel(Test)

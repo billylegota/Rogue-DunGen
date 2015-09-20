340f0eee1c1aa7497e571adf3207d3730ae4c549
billylegota <billy@texasblounts.com>
2015-09-20 13:22:14 -0500

    Update generatorFull.py

--- generatorFull.py
+++ generatorFull.py
@@ -5,11 +5,11 @@ Tiles:
 ------
 0 : Stone
 1 : Floor
+2 : Door
 """
 
 class Level(object):
-  def __init__(self, name, x, y):
-    self.name = name
+  def __init__(self, x, y):
     self.x = x
     self.y = y
     
@@ -24,12 +24,6 @@ class Level(object):
         output[x].append(0)
         
     return output
-    
-  def get(self, x, y):
-    return self.level[x][y]
-    
-  def set(self, x, y, value):
-    self.level[x][y] = value
     
   def genRoom(self, x_var=range(4,12)[1::2], y_var=range(4,12)[1::2]):
     room_top = [random.choice(range(self.x - 1)[1::2]), random.choice(range(self.y - 1)[1::2])]
@@ -65,8 +59,6 @@ class Level(object):
           for y in range(y1, y2):
             self.level[x][y] = 1
             
-  import random
-
 # Design:
 # 1. Generate an array of odd coords for use in the maze generation algorithm.
 # 2. Pick a random point in that new array that is valid and push it to the stack.
@@ -76,61 +68,57 @@ class Level(object):
 # 6. GOTO 3 unless the stack is empty.
 # Print the map
 
-class MazeGen(object):
-  def __init__(self, level):
-    self.level = level
-    self.stack = []
-    
-  def getNeighbors(self, x, y):
-    output = []
-    for dir in [[x - 2, y], [x, y - 2], [x, y + 2], [x + 2, y]]:
-      try:
-        result = self.level.get(dir[0], dir[1])
-        if result == 0 and 1 <= dir[0] < self.level.x - 1 and 1 <= dir[1] < self.level.y - 1:
-          output.append(dir)
-      except:
-        pass
-    return output
-    
-  def connect(self, p1, p2):
-    self.level.set(p2[0], p2[1], 1)
-    
-    if p1[0] == p2[0]:
-      if p1[1] > p2[1]:
-        self.level.set(p2[0], p2[1] + 1, 1)
-        
-      elif p1[1] < p2[1]:
-        self.level.set(p2[0], p2[1] - 1, 1)
-        
-    elif p1[1] == p2[1]:
-      if p1[0] > p2[0]:
-        self.level.set(p2[0] + 1, p2[1], 1)
-        
-      elif p1[0] < p2[0]:
-        self.level.set(p2[0] - 1, p2[1], 1)
-    
-    else:
-      raise IndexError
-      
-  def iterate(self):
-    current = self.stack[len(self.stack) - 1]
-    options = self.getNeighbors(current[0], current[1])
-    
-    if len(options) > 0:
-      option = random.choice(options)
-      self.connect(current, option)
-      self.stack.append(option)
-    else:
-      self.stack.pop()
-      
   def genMaze(self):
-    while True:
-      attempt = [random.choice(range(self.level.x - 1)[1::2]), random.choice(range(self.level.y - 1)[1::2])]
-      print attempt
-      if self.level.get(attempt[0], attempt[1]) == 0:
-        self.level.set(attempt[0], attempt[1], 1)
-        self.stack.append(attempt)
-        break
+    def getNeighbors(x, y):
+      output = []
+      for dir in [[x - 2, y], [x, y - 2], [x, y + 2], [x + 2, y]]:
+        try:
+          result = self.level.get(dir[0], dir[1])
+          if result == 0 and 1 <= dir[0] < self.level.x - 1 and 1 <= dir[1] < self.level.y - 1:
+            output.append(dir)
+        except:
+          pass
+      return output
+    
+    def connect(p1, p2):
+      self.level.set(p2[0], p2[1], 1)
+    
+      if p1[0] == p2[0]:
+        if p1[1] > p2[1]:
+          self.level.set(p2[0], p2[1] + 1, 1)
+        
+        elif p1[1] < p2[1]:
+          self.level.set(p2[0], p2[1] - 1, 1)
+        
+      elif p1[1] == p2[1]:
+        if p1[0] > p2[0]:
+          self.level.set(p2[0] + 1, p2[1], 1)
+        
+        elif p1[0] < p2[0]:
+          self.level.set(p2[0] - 1, p2[1], 1)
+    
+      else:
+        raise IndexError
+      
+    def iterate():
+      current = self.stack[len(self.stack) - 1]
+      options = self.getNeighbors(current[0], current[1])
+    
+      if len(options) > 0:
+        option = random.choice(options)
+        self.connect(current, option)
+        self.stack.append(option)
+      else:
+        self.stack.pop()
+      
+    def genMaze():
+      while True:
+        attempt = [random.choice(range(self.level.x - 1)[1::2]), random.choice(range(self.level.y - 1)[1::2])]
+        print attempt
+        if self.level.get(attempt[0], attempt[1]) == 0:
+          self.level.set(attempt[0], attempt[1], 1)
+          self.stack.append(attempt)
+          break
 
-    while len(self.stack) != 0:
-      self.iterate()
+      while len(self.stack) != 0:
+        iterate()

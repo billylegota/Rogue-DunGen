9aabaad585e27992b3434d3d96d21e17cce7845b
billylegota <billy@texasblounts.com>
2015-09-20 16:57:12 -0500

    Rewrote generator in one file

--- generatorFull2.py
+++ generatorFull2.py
@@ -6,6 +6,8 @@ Tiles:
 0 : Stone
 1 : Floor
 2 : Door
+4 : Down stairwell
+5 : Up stairwell
 """
 
 class Level(object):
@@ -49,47 +51,11 @@ class Level(object):
           for x in range(x1, x2):
             for y in range(y1, y2):
               self.level[x][y] = 1
-
-
-  # Maze Generator:
-  # 1. Generate an array of odd coords for use in the maze generation algorithm.
-  # 2. Pick a random point in that new array that is valid and push it to the stack.
-  # 3. Find the valid neighbors to the NSEW and select one of them. Connect the current and the new points.
-  # 4. Push new point to stack.
-  # 5. If there are no valid neighbors then pop the stack.
-  # 6. unless the stack is empty GOTO 3.
+              
   
   def genMaze(self):
     self.stack = []
-    
-    def getNeighbors(self, x, y):
-      output = []
-      for dir in [[x - 2, y], [x, y - 2], [x, y + 2], [x + 2, y]]:
-        try:
-          result = self.get(dir[0], dir[1])
-          if result == 0 and 1 <= dir[0] < self.x - 1 and 1 <= dir[1] < self.y - 1:
-            output.append(dir)
-        except:
-          pass
-      return output
-    
-    def connect(self, p1, p2):
-      self.set(p2[0], p2[1], 1)
-    
-      if p1[0] == p2[0]:
-        if p1[1] > p2[1]:
-          self.set(p2[0], p2[1] + 1, 1)
-        
-        elif p1[1] < p2[1]:
-          self.set(p2[0], p2[1] - 1, 1)
-        
-      elif p1[1] == p2[1]:
-        if p1[0] > p2[0]:
-          self.set(p2[0] + 1, p2[1], 1)
-        
-        elif p1[0] < p2[0]:
-          self.set(p2[0] - 1, p2[1], 1)    
-      
+
     while True:
       attempt = [random.choice(range(self.x - 1)[1::2]), random.choice(range(self.y - 1)[1::2])]
       print attempt
@@ -99,13 +65,41 @@ class Level(object):
         break
 
     while len(self.stack) != 0:
-      current = self.stack[len(self.stack) - 1]
-      options = getNeighbors(self, current[0], current[1])
-    
+      x1, y1 = self.stack[len(self.stack) - 1]
+
+      options = []
+      for dir in [[x1 - 2, y1], [x1, y1 - 2], [x1, y1 + 2], [x1 + 2, y1]]:
+        try:
+          result = self.get(dir[0], dir[1])
+          if result == 0 and 1 <= dir[0] < self.x - 1 and 1 <= dir[1] < self.y - 1:
+            options.append(dir)
+        except:
+          pass
+
       if len(options) > 0:
-        option = random.choice(options)
-        connect(self, current, option)
-        self.stack.append(option)
+        x2, y2 = random.choice(options)
+        self.set(x2, y2, 1)
+        self.stack.append([x2, y2])
+    
+        if x1 == x2:
+          if y1 > y2:
+            self.set(x2, y2 + 1, 1)
+        
+          elif y1 < y2:
+            self.set(x2, y2 - 1, 1)
+        
+        elif y1 == y2:
+          if x1 > x2:
+            self.set(x2 + 1, y2, 1)
+        
+          elif x1 < x2:
+            self.set(x2 - 1, y2, 1)
       else:
         self.stack.pop()
+
+  def unify(self):
+    pass # This is where the region unification code will go
+
+  def placeStairs(self, number):
+    pass # This is where the stairwells will be placed
 
